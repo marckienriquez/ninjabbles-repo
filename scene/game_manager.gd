6ad8f4196f7bcdab39b5db96bfb05c00110e6609
Austiel <abdielaustero.work@gmail.com>
2026-04-05 18:10:35 +0800

    feat: code block editor updates

--- scene/game_manager.gd
+++ scene/game_manager.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+@onready var panel = $CanvasLayer/Panel
+
 func _ready():
 	$avatar_greenie.connect("character_clicked", self._on_character_clicked)
 
 func _on_character_clicked():
-	$CanvasLayer/Panel.visible = true
+	panel.visible = true
+
+
+func _on_button_pressed() -> void:
+	panel.visible = false
