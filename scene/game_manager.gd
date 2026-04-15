3552090bfe4b09299f5f49aaf5e550c647ded52a
Austiel <abdielaustero.work@gmail.com>
2026-04-15 12:30:14 +0800

    fix: ui fix

--- scene/game_manager.gd
+++ scene/game_manager.gd
@@ -7,7 +7,7 @@ func _ready():
 	$avatar_greenie.connect("character_clicked", self._on_character_clicked)
 	$CanvasLayer/BlockEditor/CloseEditorButton.connect("pressed", self._on_button_pressed)
 	$CanvasLayer/BlockEditor/RunButton.connect("pressed", self._on_button_pressed)
-	$CanvasLayer/Summary/CloseSummaryButton.connect("pressed", self._on_summary_button_pressed)
+	$CanvasLayer/Summary/summary_screen/CloseSummaryButton.connect("pressed", self._on_summary_button_pressed)
 
 func _on_character_clicked():
 	editor.visible = true
