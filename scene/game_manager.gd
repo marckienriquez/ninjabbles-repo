82dc9431b4bd082d4b182ec08563bb75cf687fe7
Austiel <abdielaustero.work@gmail.com>
2026-04-15 09:28:01 +0800

    fix: ui fix

--- scene/game_manager.gd
+++ scene/game_manager.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
-@onready var panel = $CanvasLayer/BlockEditor
+@onready var editor = $CanvasLayer/BlockEditor
+@onready var summary = $CanvasLayer/Summary
 
 func _ready():
 	$avatar_greenie.connect("character_clicked", self._on_character_clicked)
 	$CanvasLayer/BlockEditor/CloseEditorButton.connect("pressed", self._on_button_pressed)
 	$CanvasLayer/BlockEditor/RunButton.connect("pressed", self._on_button_pressed)
+	$CanvasLayer/Summary/CloseSummaryButton.connect("pressed", self._on_summary_button_pressed)
 
 func _on_character_clicked():
-	panel.visible = true
+	editor.visible = true
 
 
 func _on_button_pressed() -> void:
-	panel.visible = false
+	editor.visible = false
+	
+func _on_summary_button_pressed() -> void:
+	summary.visible = false
