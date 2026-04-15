9ab01237b03c505ce075b6785ca0df7108e14190
Austiel <abdielaustero.work@gmail.com>
2026-04-15 08:48:06 +0800

    feat: script and ui changes

--- scene/game_manager.gd
+++ scene/game_manager.gd
@@ -4,6 +4,8 @@ extends Node2D
 
 func _ready():
 	$avatar_greenie.connect("character_clicked", self._on_character_clicked)
+	$CanvasLayer/BlockEditor/CloseEditorButton.connect("pressed", self._on_button_pressed)
+	$CanvasLayer/BlockEditor/RunButton.connect("pressed", self._on_button_pressed)
 
 func _on_character_clicked():
 	panel.visible = true
