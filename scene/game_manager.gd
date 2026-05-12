dff4b05bb8da268b6456a9bc4ea3a05a8250c85b
Austiel <abdielaustero.work@gmail.com>
2026-05-13 01:38:28 +0800

    feat: added timer and retry function

--- scene/game_manager.gd
+++ scene/game_manager.gd
@@ -8,7 +8,7 @@ func _ready():
 	$avatar_greenie.connect("character_clicked", self._on_character_clicked)
 	$CanvasLayer/BlockEditor/CloseEditorButton.connect("pressed", self._on_button_pressed)
 	$CanvasLayer/BlockEditor/RunButton.connect("pressed", self._on_button_pressed)
-	$CanvasLayer/Instructions/CloseInstructionsButton.connect("pressed", self._on_instructions_button_pressed)
+	$CanvasLayer/Instructions/InstructionsTexture/CloseInstructionsButton.connect("pressed", self._on_instructions_button_pressed)
 	$CanvasLayer/Summary/summary_screen/CloseSummaryButton.connect("pressed", self._on_summary_button_pressed)
 
 func _on_character_clicked():
