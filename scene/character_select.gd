f54168bf88928c59602ce94c48f542101f0e224f
zumeragi <reaganrylie.fabros@gmail.com>
2026-05-18 14:17:08 +0800

    fix screen size prob

--- scene/character_select.gd
+++ scene/character_select.gd
@@ -1,18 +1,18 @@
-extends Node2D
+extends Control
 
-@onready var big_sprite: AnimatedSprite2D = $Control/AnimatedSprite2D
-@onready var char_name: Label = $Control/char_name
-@onready var char_desc: Label = $Control/char_desc
-@onready var in_use: Label = $Control/in_use
+@onready var big_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var char_name: Label = $char_name
+@onready var char_desc: Label = $char_desc
+@onready var in_use: Label = $in_use
 
-@onready var current_char: TextureButton = $Control/current_char
-@onready var prev_char: TextureButton = $Control/prev_char
-@onready var next_char: TextureButton = $Control/next_char
+@onready var current_char: TextureButton = $current_char
+@onready var prev_char: TextureButton = $prev_char
+@onready var next_char: TextureButton = $next_char
 
-@onready var move_right: TextureButton = $Control/move_right
-@onready var move_left: TextureButton = $Control/move_left
-@onready var back_button: TextureButton = $Control/Back
-@onready var play_button: TextureButton = $Control/Play
+@onready var move_right: TextureButton = $move_right
+@onready var move_left: TextureButton = $move_left
+@onready var back_button: TextureButton = $Back
+@onready var play_button: TextureButton = $Play
 
 var current_index := 0
 
@@ -43,7 +43,7 @@ var characters = [
 	},
 	{
 		"name": "FURUI",
-		"desc": "Furui sensei is considered one of the top-ranking ninjas in “The Clan,” tasked with leading and guiding the rookies towards their success in the mission. Imparts wisdom that the group hangs on to.",
+		"desc": "Furui sensei is considered one of the top-ranking ninjas in 'The Clan,' tasked with leading and guiding the rookies towards their success in the mission. Imparts wisdom that the group hangs on to.",
 		"frames": preload("res://asset/sprites/furui_idle.tres"),
 		"icon": preload("res://asset/others/furui.png")
 	},
@@ -58,11 +58,19 @@ var characters = [
 func _ready():
 	move_right.pressed.connect(_on_move_right_pressed)
 	move_left.pressed.connect(_on_move_left_pressed)
+
 	next_char.pressed.connect(_on_next_char_pressed)
 	prev_char.pressed.connect(_on_prev_char_pressed)
 	current_char.pressed.connect(_on_current_char_pressed)
+
 	back_button.pressed.connect(_on_back_pressed)
 	play_button.pressed.connect(_on_play_pressed)
+
+	# Restore previously selected character
+	for i in range(characters.size()):
+		if characters[i]["name"] == GlobalData.selected_character:
+			current_index = i
+			break
 
 	update_character()
 
@@ -81,7 +89,11 @@ func update_character():
 		in_use.text = ""
 
 	big_sprite.sprite_frames = selected["frames"]
-	big_sprite.play("idle")
+
+	if big_sprite.animation != "idle":
+		big_sprite.play("idle")
+	else:
+		big_sprite.play()
 
 	current_char.texture_normal = selected["icon"]
 	prev_char.texture_normal = characters[prev_index]["icon"]
@@ -108,7 +120,8 @@ func _on_prev_char_pressed():
 	previous_character()
 
 func _on_current_char_pressed():
-	pass
+	GlobalData.selected_character = characters[current_index]["name"]
+	update_character()
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scene/main_scrn/main_screen.tscn")
@@ -117,3 +130,13 @@ func _on_play_pressed():
 	GlobalData.selected_character = characters[current_index]["name"]
 	update_character()
 	get_tree().change_scene_to_file("res://scene/easy_screen.tscn")
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_right"):
+		next_character()
+
+	elif event.is_action_pressed("ui_left"):
+		previous_character()
+
+	elif event.is_action_pressed("ui_accept"):
+		_on_play_pressed()
